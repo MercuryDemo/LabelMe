208035e8e91f03a8e57500b3feeb2a7b10055843
MercuryDemo <81722899+MercuryDemo@users.noreply.github.com>
2022-01-05 22:27:20 +0800

    delete django

--- DjangoLabel/labelme/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LabelmeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'labelme'
--- DjangoLabel/labelme/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from labelme import views
-
-urlpatterns = [
-      path('newhello/', views.newhello),
-]
